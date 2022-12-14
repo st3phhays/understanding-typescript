c5d0326a1fa1187d7382aab4b25497d43481faac
Stephanie Hays <steph@stephhays.dev>
2022-12-14 11:15:56 -0600

    Section 2: Module 16: Object Types

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,16 +1,10 @@
-const add = (n1: number, n2: number, showResult: boolean, phrase: string) => {
-    const result = n1 + n2;
+// const person: {
+    // name: string;
+    // age: number;
+// } = {
+const person = {
+    name: 'Steph',
+    age: 32
+}
 
-    if (showResult) {
-        console.log(`${phrase}${result}`)
-    } else {
-        return result;
-    }
-};
-
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person.name);

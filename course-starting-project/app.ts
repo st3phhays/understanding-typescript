a472e2bb5e8c13d880934e820f7fac3ad9f68090
Stephanie Hays <steph@stephhays.dev>
2022-12-14 11:28:42 -0600

    Section 2: Module 19: Tuples

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -2,11 +2,21 @@
     // name: string;
     // age: number;
 // } = {
-const person = {
+const person: {
+    name: string;
+    age: number, 
+    hobbies: string[],
+    role: [number, string] // tuple type
+} = {
     name: 'Steph',
     age: 32,
-    hobbies: ['Coding', 'Gaming']
+    hobbies: ['Coding', 'Gaming'],
+    role: [2, 'developer']
 }
+
+// person.role.push('admin');
+//person.role[1] = 10;  !! ERROR  !!
+//person.role = [0, 'admin', 'user'] !! ERROR !!
 
 let favoriteActivities: string[];
 favoriteActivities = ['Coding'];

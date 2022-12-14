38e1ae2a22ed635528892bf45f8ad84d9fb6d9e8
Stephanie Hays <steph@stephhays.dev>
2022-12-14 13:43:37 -0600

    Section 2: Module 29: "unknown" Type

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,28 +1,10 @@
-const add = (n1: number, n2: number) => {
-    return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Steph';
+//userName = userInput; !! ERROR !!
+
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-
-// void return type. Doesn't return anything.
-const printResult = (num: number) => {
-    console.log('Result: ' + num);
-}
-
-const addAndHandle = (n1: number, n2: number, cb: (num: number) => void) => {
-    const result = n1 + n2;
-    cb(result);
-}
-
-printResult(add(5, 20));
-
-// Describes what type of function is allowed to be used
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-// combineValues = printResult !! ERROR !!
-// combineValues === 5; !! ERROR !!
-
-console.log(combineValues(5, 20));
-
-addAndHandle(10, 20, (result) => {
-    console.log(result);
-});

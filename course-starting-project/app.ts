52ae4468b2d3ed64084afb394fb61db7c9c26029
Stephanie Hays <steph@stephhays.dev>
2022-12-14 13:08:11 -0600

    Section 2: Module 24: Type Aliases / Custom Types

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,4 +1,8 @@
-const combine = (input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text') => {
+
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+const combine = (input1: Combinable, input2: Combinable, resultConversion: ConversionDescriptor) => {
     let result;
 
     if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
